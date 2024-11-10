faf480686dd954a6b105017567615e1610e73c23
Kushagra Anit Singh <kushagraa.n@gmail.com>
2024-11-10 11:19:05 +0530

    server issue res

--- client/src/components/AccessDenied.js
+++ client/src/components/AccessDenied.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './AccessDenied.css'; // Import the CSS file
+import './AccessDenied.css';
 
 const AccessDenied = () => (
     <div className="access-denied-container">

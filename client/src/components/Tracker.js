5a7f2b9dae5ae7a364715102e337292b72d4dcac
BrandonCerejo <118354029+BrandonCerejo@users.noreply.github.com>
2024-11-10 23:54:54 +0530

    nutrition css

--- client/src/components/Tracker.js
+++ client/src/components/Tracker.js
@@ -16,8 +16,7 @@ const Tracker = () => {
         const data = [
             { type: 'Chest', imageUrl: 'https://images.unsplash.com/photo-1571019614242-c5c5dee9f50b?ixlib=rb-4.0.3&auto=format&fit=crop&w=1740&q=80' },
             { type: 'Abs', imageUrl: 'https://images.unsplash.com/photo-1571019613454-1cb2f99b2d8b?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=60' },
-            { type: 'Shoulder', imageUrl: 'https://images.unsplash.com/photo-1532029837206-abbe2b7620e3?ixlib=rb-4.0.3&auto=format&fit=crop&w=1740&q=80' },
-            { type: 'Back', imageUrl: 'https://images.unsplash.com/photo-1603287681836-b174ce5074c2?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=60' },
+            // { type: 'Back', imageUrl: 'https://images.unsplash.com/photo-1603287681836-b174ce5074c2?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=60' },
             { type: 'Biceps', imageUrl: 'https://images.unsplash.com/photo-1581009146145-b5ef050c2e1e?ixlib=rb-4.0.3&auto=format&fit=crop&w=1740&q=80' },
             { type: 'Triceps', imageUrl: 'https://images.unsplash.com/photo-1530822847156-5df684ec5ee1?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=60' },
             { type: 'Legs', imageUrl: 'https://images.unsplash.com/photo-1434608519344-49d77a699e1d?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=60' },
@@ -70,78 +69,84 @@ const Tracker = () => {
     };
 
     return (
-        <div className="tracker-page">
-            {/* Search Bar for Food Tracking */}
-            <div className="search-bar">
-                <input
-                    type="text"
-                    className="search-input"
-                    placeholder="Search food..."
-                    value={searchQuery}
-                    onChange={(e) => setSearchQuery(e.target.value)}
-                />
-            </div>
-
-            {/* Display Nutritional Information Form for the First Search Result */}
-            {foodData && (
-                <div className="food-info">
-                    <h2>{foodData.description}</h2>
-                    <form className="food-form">
-                        <label>
-                            Calories:
-                            <input type="text" value={`${foodData.calories} kcal`} readOnly />
-                        </label>
-                        <label>
-                            Protein:
-                            <input type="text" value={`${foodData.protein} g`} readOnly />
-                        </label>
-                        <label>
-                            Fat:
-                            <input type="text" value={`${foodData.fat} g`} readOnly />
-                        </label>
-                        <label>
-                            Meal Type:
-                            <select value={mealType} onChange={(e) => setMealType(e.target.value)}>
-                                <option value="">Select Meal</option>
-                                <option value="Breakfast">Breakfast</option>
-                                <option value="Lunch">Lunch</option>
-                                <option value="Dinner">Dinner</option>
-                            </select>
-                        </label>
-                        <button type="button" onClick={logFood} disabled={!mealType}>Log This Food</button>
-                    </form>
+        <div className="tracker">
+            <div className="tracker-container">
+                {/* Search Bar for Food Tracking */}
+                <h2>Nutrition Tracker</h2>
+                <div className="search-bar">
+                    <input
+                        type="text"
+                        className="search-input"
+                        placeholder="Search food..."
+                        value={searchQuery}
+                        onChange={(e) => setSearchQuery(e.target.value)}
+                    />
                 </div>
-            )}
 
-            {/* Logged Foods */}
-            <h2>Logged Foods</h2>
-            <ul>
-                {loggedFoods.map((food, index) => (
-                    <li key={index}>
-                        {food.description}: {food.calories} kcal, {food.protein}g protein, {food.fat}g fat 
-                        (Logged on: {food.loggedDate}, Meal: {food.mealType})
-                    </li>
-                ))}
-            </ul>
-
-            {/* Workout Cards */}
-            <h1 className="main-heading">Workouts</h1>
-            <div className="workouts-container">
-                {workouts.map((item, index) => (
-                    <div
-                        className="workout-card"
-                        key={index}
-                        onClick={() => navigateToWorkout(item.type)}
-                    >
-                        <div
-                            className="workout-card-image"
-                            style={{ backgroundImage: `url(${item.imageUrl})` }}
-                        ></div>
-                        <h2 className="workout-card-title">{item.type}</h2>
+                {/* Display Nutritional Information Form for the First Search Result */}
+                {foodData && (
+                    <div className="food-info">
+                        <h2>{foodData.description}</h2>
+                        <form className="food-form">
+                            <label>
+                                Calories:
+                                <input type="text" value={`${foodData.calories} kcal`} readOnly />
+                            </label>
+                            <label>
+                                Protein:
+                                <input type="text" value={`${foodData.protein} g`} readOnly />
+                            </label>
+                            <label>
+                                Fat:
+                                <input type="text" value={`${foodData.fat} g`} readOnly />
+                            </label>
+                            <label>
+                                Meal Type:
+                                <select value={mealType} onChange={(e) => setMealType(e.target.value)}>
+                                    <option value="">Select Meal</option>
+                                    <option value="Breakfast">Breakfast</option>
+                                    <option value="Lunch">Lunch</option>
+                                    <option value="Dinner">Dinner</option>
+                                </select>
+                            </label>
+                            <button type="button" onClick={logFood} disabled={!mealType}>Log This Food</button>
+                        </form>
                     </div>
-                ))}
+                )}
+
+                {/* Logged Foods */}
+                <h2>Logged Foods</h2>
+                <div className="logged-foods-container">
+                    <ul className="logged-foods-list">
+                        {loggedFoods.map((food, index) => (
+                            <li key={index}>
+                                <span>{food.description}</span>: {food.calories} kcal, {food.protein}g protein, {food.fat}g fat 
+                                <div className="meal-type">({food.mealType})</div>
+                                <div className="logged-date">Logged on: {food.loggedDate}</div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
-        </div>
+                {/* Workout Cards */}
+                <h1 className="main-heading">Workouts</h1>
+                <div className="workouts-container">
+                    {workouts.map((item, index) => (
+                        <div
+                            className="workout-card"
+                            key={index}
+                            onClick={() => navigateToWorkout(item.type)}
+                        >
+                            <div
+                                className="workout-card-image"
+                                style={{ backgroundImage: `url(${item.imageUrl})` }}
+                            ></div>
+                            <h2 className="workout-card-title">{item.type}</h2>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        
     );
 };
 
